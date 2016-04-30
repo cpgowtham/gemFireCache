120e6a4b835e47f59d5c96b35797596229e2e795
Gowtham <gowthamdexter@outlook.com>
2016-04-30 23:36:33 +0530

    commit

--- gemFireCache/src/com/client/ClientCacheOne.java
+++ gemFireCache/src/com/client/ClientCacheOne.java
@@ -2,30 +2,26 @@ package com.client;
 
 import java.util.Properties;
 
-import com.gemstone.gemfire.cache.InterestResultPolicy;
 import com.gemstone.gemfire.cache.Region;
 import com.gemstone.gemfire.cache.client.ClientCache;
 import com.gemstone.gemfire.cache.client.ClientCacheFactory;
 import com.pojo.Employee;
 
-public class CacheClient {
+public class ClientCacheOne {
 
 	public static void main(String[] args) {
-
+		
 		Properties prop = new Properties();
 
-		prop.setProperty("cache-xml-file", "client.xml");
+		prop.setProperty("cache-xml-file", "clientOne.xml");
 
 		ClientCache cache = new ClientCacheFactory(prop).create();
 
 		Region<String, Employee> empRegion = cache.getRegion("employee");
 
-		empRegion.registerInterest("ALL_KEYS", InterestResultPolicy.KEYS);
-
-		//empRegion.put("001", new Employee("user1", "pwd2"));
+		empRegion.put("001", new Employee("user1", "pwd2"));
 		
-		while(true){
-		}
-	}
+		
 
+	}
 }
