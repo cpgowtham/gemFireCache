81d7719ca32721f7e833ba96133780333df807a3
Gowtham <gowthamdexter@outlook.com>
2016-04-30 23:17:12 +0530

    commit

--- gemFireCache/src/com/client/CacheClient.java
+++ gemFireCache/src/com/client/CacheClient.java
@@ -8,28 +8,23 @@ import com.gemstone.gemfire.cache.client.ClientCache;
 import com.gemstone.gemfire.cache.client.ClientCacheFactory;
 import com.pojo.Employee;
 
-public class CacheClient implements Runnable{
+public class CacheClient {
 
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) {
+
 		Properties prop = new Properties();
-		
+
 		prop.setProperty("cache-xml-file", "client.xml");
-		
+
 		ClientCache cache = new ClientCacheFactory(prop).create();
 
 		Region<String, Employee> empRegion = cache.getRegion("employee");
-			
-		//empRegion.registerInterest("ALL_KEYS", InterestResultPolicy.KEYS);
-		
+
+		empRegion.registerInterest("ALL_KEYS", InterestResultPolicy.KEYS);
+
 		empRegion.put("001", new Employee("user1", "pwd1"));
 		empRegion.put("002", new Employee("user2", "pwd2"));
-		
-		
-		
+
 	}
-	
-	
 
 }
